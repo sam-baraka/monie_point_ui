407b59d99867457b6d4f175dba729ef4220abb56
Samuel Baraka <sababuvercetti@gmail.com>
2024-05-11 18:04:01 +0300

    Add colors

---
+++ lib/resources/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color black = const Color(0x0ff23220);
+  static Color grey = const Color(0xFF737373);
+  static Color orange = const Color(0xFFfc9d11);
+
+}
